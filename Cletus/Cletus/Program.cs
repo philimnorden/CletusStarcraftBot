fad3a772bf0135a35658b0a7b46ea3971f94051b
Philip Herder <philotheking@gmx.net>
2018-06-08 23:33:07 +0200

    Added Main Loop

--- Cletus/Cletus/Program.cs
+++ Cletus/Cletus/Program.cs
@@ -1,4 +1,5 @@
-﻿using SC2APIProtocol;
+﻿using Google.Protobuf.Collections;
+using SC2APIProtocol;
 using Starcraft2;
 using System;
 using System.Collections.Generic;
@@ -6,8 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Action = SC2APIProtocol.Action;
+
+
 namespace Cletus
 {
+
+    public enum BuildingID
+    {
+        CommandCenter = 18,
+    }
+
+    public enum BuildingTrainingID
+    {
+        SCV = 524,
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -23,7 +38,7 @@ namespace Cletus
         Sc2Game.Participant.CreateParticipant(
             createInstance(),
             Race.Terran,
-            (state => (IEnumerable<SC2APIProtocol.Action>)new SC2APIProtocol.Action[] {})),
+            MasterAgent_MainLoop),
         Sc2Game.Participant.CreateComputer(Race.Terran, Difficulty.Hard)
     };
 
@@ -34,6 +49,43 @@ namespace Cletus
 
             // Runs the game to the end with the given bots / map and configuration
             Runner.run(Sc2Game.runGame(gameSettings, participants));
+
+            var x = new Observation();
+            Console.WriteLine(x.PlayerCommon.Minerals);
+
         }
+
+        public static IEnumerable<Action> MasterAgent_MainLoop(GameState gameState)
+        {
+            Action answer = new Action();
+
+            Observation newObservation = gameState.NewObservation.Observation;
+            MapState map = newObservation.RawData.MapState;
+
+            RepeatedField<Unit> allUnits = newObservation.RawData.Units;
+
+            ulong? unitTag = null;
+
+            int i = 0;
+
+            while (unitTag == null && i < allUnits.Count)
+            {
+                if (allUnits[i].UnitType == (int)BuildingID.CommandCenter)
+                {
+                    unitTag = allUnits[i].Tag;
+                }
+                i++;
+            }
+
+            answer.ActionRaw = new ActionRaw();
+            answer.ActionRaw.ClearAction();
+            answer.ActionRaw.UnitCommand = new ActionRawUnitCommand();
+            answer.ActionRaw.UnitCommand.AbilityId = (int)BuildingTrainingID.SCV;
+
+            answer.ActionRaw.UnitCommand.UnitTags.Add(unitTag.Value);
+
+            yield return answer;
+        }
+
     }
 }
